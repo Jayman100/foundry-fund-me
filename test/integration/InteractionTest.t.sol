2bcc5601013a5bf339163380190c4cbd88aae5fe
Garba Jamiu <garbajamiu2000@gmail.com>
2024-12-06 11:39:00 +0100

    create an interaction test

---
+++ test/integration/InteractionTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import {Test, console} from "../../lib/forge-std/src/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interaction.s.sol";
+
+contract InteractionTest is Test {
+    FundMe fundMe;
+
+    // Create a fake address just for testing
+    address USER = makeAddr("jayman");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteraction() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assertEq(address(fundMe).balance, 0);
+    }
+}
